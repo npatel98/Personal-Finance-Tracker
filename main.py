de47101b9b52ce59cee35191b18e30af05567bca
Nikhil Patel <nikhil.patel@colorado.edu>
2024-07-29 20:54:53 -0500

    create  get_transactions classmethod

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from data_entry import get_amount, get_category, get_date, get_description
 class CSV:
     CSV_FILE = "finance_data.csv"
     COLUMNS = ['date', 'amount', 'category', 'description']
+    FORMAT = "%d-%m-%Y"
 
     @classmethod
     def initialize_csv(cls):
@@ -32,7 +33,32 @@ class CSV:
             writer.writerow(new_entry)
         print("Entry added successfully")
 
-def add():
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df["date"] = pd.to_datetime(df["date"], format=CSV.FORMAT)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+        end_date = datetime.strptime(end_date, CSV.FORMAT)
+        
+        mask = (df["date"] >= start_date) & (df["date"] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print("No transactions found in the given date range.")
+        else:
+            print(f"Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}")
+            print(filtered_df.to_string(index=False, formatters={"date": lambda x: x.strftime(CSV.FORMAT)}))
+
+            total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
+            total_expense = filtered_df[filtered_df["category"] == "Expense"]["amount"].sum()
+            print("\nSummary:")
+            print(f"Total Income: ${total_income:.2f}")
+            print(f"Total Expense: ${total_expense:.2f}")
+            print(f"Total Savings: ${(total_income - total_expense):.2f}")
+
+            return filtered_df
+
+def add()
     CSV.initialize_csv()
     date = get_date("Enter the date of the transaction (dd-mm-yyyy) or enter for today's date: ", allow_default=True)
     amount = get_amount()
